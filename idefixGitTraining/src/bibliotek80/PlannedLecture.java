b3326d80a370be45181f635f0e528b900956708c
hothes <hothes@yahoo.com>
2012-03-14 11:14:45 +0100

    test 1

--- idefixGitTraining/src/bibliotek80/PlannedLecture.java
+++ idefixGitTraining/src/bibliotek80/PlannedLecture.java
@@ -11,7 +11,7 @@ public class PlannedLecture {
 	//Louise Test HALLO
 	//Test Carsten
 	//Test
-	//Test Sarah
+	//Test Sarah 
 	private Lecture lecture;
 	private String forum;
 	private Date calendar;
