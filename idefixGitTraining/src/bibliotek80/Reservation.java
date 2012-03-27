0bef6fd3541e7556dc56c98996ffa274d94f267b
ianjamesgoodwin <ijgcompany@me.com>
2012-03-27 19:32:10 +0200

    Commit for Reservation class attempt 2

--- idefixGitTraining/src/bibliotek80/Reservation.java
+++ idefixGitTraining/src/bibliotek80/Reservation.java
@@ -1,10 +1,16 @@
 package bibliotek80;
 
-public class Reservation {
+import org.deformation.utilities.NewGregorianCalendar;
 
+public class Reservation {
+	private Bog bog;
+	private NewGregorianCalendar dato;
+	private Person laaner;
 	public Reservation(Person person, Bog titel, int y, int m, int d) {
 		// TODO Auto-generated constructor stub
-	
+		this.laaner=person;
+		this.dato= new NewGregorianCalendar (y,m,d);
+		this.bog=titel;
 	
 	}
 
