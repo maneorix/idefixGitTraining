93200e1b90ae065a1cf15f76deb973c24fca4e5a
idespring12 <nml@eaaa.dk>
2012-03-27 23:20:30 +0300

    Update idefixGitTraining/src/bibliotek80/Reservation.java

--- idefixGitTraining/src/bibliotek80/Reservation.java
+++ idefixGitTraining/src/bibliotek80/Reservation.java
@@ -4,14 +4,24 @@ import org.deformation.utilities.NewGregorianCalendar;
 
 public class Reservation {
 	private Bog bog;
-	private NewGregorianCalendar dato;
+	private NewGregorianCalendar dato; //supplied by teacher
 	private Person laaner;
+
+
 	public Reservation(Person person, Bog titel, int y, int m, int d) {
-		// TODO Auto-generated constructor stub
-		this.laaner=person;
-		this.dato= new NewGregorianCalendar (y,m,d);
-		this.bog=titel;
-	
+		this.laaner = person;
+		this.bog = titel;
+		this.dato = new NewGregorianCalendar (y,m,d);
 	}
+	
+	public NewGregorianCalendar getDato() { 
+	    return this.dato;
+	}
+    
+	
+	public String toString() {
+	String s = "Dato: " + this.getDato();
+	return s;
+    }
 
 }
