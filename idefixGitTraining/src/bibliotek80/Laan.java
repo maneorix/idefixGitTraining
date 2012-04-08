9e7e45faa2d5411713712e0e960e8efe70b5bb8f
maneorix <ryantomasson@gmail.com>
2012-04-08 13:47:55 +0300

    Update idefixGitTraining/src/bibliotek80/Laan.java

--- idefixGitTraining/src/bibliotek80/Laan.java
+++ idefixGitTraining/src/bibliotek80/Laan.java
@@ -1,7 +1,7 @@
 package bibliotek80;
 import org.deformation.utilities.*;
 /**
- * 
+ * hej
  * @author 
  *
  */
@@ -27,11 +27,11 @@ public class Laan {
 	}
 	
 	public String toString(){
-		String s = "Låner: " + this.laaner.getNavn() + "<br /> ";
+		String s = "Lï¿½ner: " + this.laaner.getNavn() + "<br /> ";
 		s += "Bog: " + this.bog + "<br />";
-		s += "Udlåningsdato: " + this.datoUd + "</br >";
+		s += "Udlï¿½ningsdato: " + this.datoUd + "</br >";
 		s += "Afleveringsdato: " + this.datoHjem + "<br />";
-		s += "Bøde: " + this.boede + "<br />";
+		s += "Bï¿½de: " + this.boede + "<br />";
 		
 		return s;
 	}
